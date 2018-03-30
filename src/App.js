8d02f40b3761122bffeb3bb9084ca8c2c8e3d08e
terrierscript <13282103+terrierscript@users.noreply.github.com>
2018-03-30 11:45:07 +0900

    normal counter

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+const { Provider, Consumer } = React.createContext();
 
 class App extends Component {
+  state = {
+    count: 0
+  };
+  increment = () => {
+    this.setState({
+      count: this.state.count + 1
+    });
+  };
+  decrement = () => {
+    this.setState({
+      count: this.state.count - 1
+    });
+  };
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div style={{ padding: "1em" }}>
+        <h1>Parent</h1>
+        <div>{this.state.count}</div>
+        <button onClick={this.increment}>+</button>
+        <button onClick={this.decrement}>-</button>
       </div>
     );
   }
